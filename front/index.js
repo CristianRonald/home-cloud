22d686df844fa346d4dabffe4c5c6f248f5e26ed
DEID <porta.cristianlo@gmail.com>
2023-10-26 00:33:30 -0500

    agrendo los iconos de los archivos

--- front/index.js
+++ front/index.js
@@ -12,11 +12,27 @@
       caja_tree.appendChild(dir);
       });
     }
+    crearArchivos(archivos){
+      archivos.forEach(elem => {
+      let ar = document.createElement('div');
+      ar.className = 'square';
+      const img = document.createElement('img');
+      img.src = './images/file.png'
+      const label = document.createElement('label');
+      label.innerText = elem.nombre;
+      ar.appendChild(img);
+      ar.appendChild(label);
+      caja_tree.appendChild(ar);
+      });
+    }
     async obtenerData(){
       try {
        const respuesta = await((await fetch('/api/tree')).json());
        this.crearDirectorios(
           respuesta.filter(elem=>elem.tipo==='directorio')
+          );
+       this.crearArchivos(
+          respuesta.filter(elem=>elem.tipo==='archivo')
           );
       } catch (err) {
         console.log(err);       
